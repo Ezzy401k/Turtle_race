9741e7d0e31455b23cd742eb72921e510e90fb52
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-12 17:53:27 +0300

    added formal comments

--- Turtle-race.py
+++ Turtle-race.py
@@ -1,17 +1,22 @@
 import turtle
 import random
 
+# Set up the screen
 screen = turtle.Screen()
 screen.setup(500, 400)
 
+# List of available colors for turtles
 colors = ['red', 'orange', 'green', 'blue', 'yellow', 'purple']
 
-bet = screen.textinput("make your bet", "which turtle will win the race? enter color: ")
+# Ask the user to make a bet on the winning turtle
+bet = screen.textinput("Make your bet", "Which turtle will win the race? Enter color: ")
+
+# Ensure that the user's bet is a valid color
 correct = True
 while correct:
-    bet = screen.textinput("make your bet", "which turtle will win the race? enter color: ")
+    bet = screen.textinput("Make your bet", "Which turtle will win the race? Enter color: ")
     if bet in colors:
-
+        # If the bet is valid, set the user's bet
         user_bet = (bet, bet)
         coordinates = [-100, -60, -20, 20, 60, 100]
 
@@ -21,6 +26,7 @@ while correct:
 
         turtles = []
 
+        # Create turtles with different colors and initial positions
         for i in range(6):
             new_turtle = turtle.Turtle()
             new_turtle.penup()
@@ -29,9 +35,12 @@ while correct:
             new_turtle.goto(-235, coordinates[i])
             turtles.append(new_turtle)
 
+        # Start the race
         while is_race_on:
             for j in turtles:
+                # Check if any turtle has reached the finish line
                 if j.xcor() > 230:
+                    # Check if the winning turtle matches the user's bet
                     if j.color() == user_bet:
                         print(f"You win. The {bet} turtle is the winner.")
                     else:
@@ -39,9 +48,12 @@ while correct:
                     is_race_on = False
                     correct = False
                     break
+                # Move each turtle forward by a random distance
                 random_distance = random.randint(0, 10)
                 j.forward(random_distance)
     else:
+        # Prompt the user to enter a valid color if the input is invalid
         print("Please enter a valid color!")
 
+# Close the screen when clicked
 screen.exitonclick()
